068c4d61501083fe2953974e6d40b157105f0977
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-04 13:44:49 +0200

    added untracked files

---
+++ findingEnemy/reack-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchingPage from './components/SearchingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchingPage></SearchingPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ spring_react_chatapp/chat-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChatRoom from './component/ChatRoom';
+function App() {
+  return (
+    <div className="App">
+      <ChatRoom></ChatRoom>
+    </div>
+  );
+}
+
+export default App;
