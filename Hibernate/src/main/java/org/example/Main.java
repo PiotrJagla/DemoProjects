70718be82cd6f55425b68aa8a4d32e9a14b64798
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-05 11:54:45 +0200

    JPA and hibernate working

--- Hibernate/src/main/java/org/example/Main.java
+++ Hibernate/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import org.hibernate.Session;
+
 import javax.persistence.*;
 import java.lang.reflect.Type;
 import java.time.LocalDate;
@@ -10,9 +12,31 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        hibernate();
+
+
+    }
+    public static void hibernate() {
+        EntityManagerFactory etf = Persistence.createEntityManagerFactory("test-unit");
+        EntityManager em = etf.createEntityManager();
+        Session s = em.unwrap(Session.class);
+        s.beginTransaction();
+
+        //Create
+//        User user = new User("Deeel", LocalDate.now());
+//        s.persist(user);
+
+        //Read
+        List<User> users = s.createQuery("SELECT u FROM User u", User.class).list();
+        users.forEach(System.out::println);
+
+
+        s.getTransaction().commit();
+        s.close();
+    }
+    public static void JPA() {
 
         EntityManagerFactory etf = Persistence.createEntityManagerFactory("test-unit");
-
         //CREATE
         EntityManager em = etf.createEntityManager();
         EntityTransaction et = null;
@@ -36,13 +60,13 @@ public class Main {
 
         //READ ONE
         em = etf.createEntityManager();
-        String query = "SELECT u FROM users u WHERE u.id = 1";
+        String query = "SELECT u FROM User u WHERE u.id = :userId";
         TypedQuery<User> tq = em.createQuery(query, User.class);
-        tq.setParameter("id", 1);
+        tq.setParameter("userId", 1);
         User gettedUser = null;
         try{
             gettedUser = tq.getSingleResult();
-            System.out.println(gettedUser.getId() + " " + gettedUser.getName() + " " + gettedUser.getBirthDate().toString());
+            System.out.println(gettedUser);
 
         } catch (NoResultException e) {
             System.out.println(e.getMessage());
@@ -52,7 +76,7 @@ public class Main {
 
         //READ MULTIPLE
         em = etf.createEntityManager();
-        String allUsersQuery = "SELECT u FROM users u";
+        String allUsersQuery = "SELECT u FROM User u";
         tq = em.createQuery(allUsersQuery, User.class);
         List<User> users;
         try {
@@ -72,7 +96,7 @@ public class Main {
             et = em.getTransaction();
             et.begin();
             User user = null;
-            user = em.find(User.class, 1);
+            user = em.find(User.class, 5);
             user.setName("NOWE IMIE");
             em.persist(user);
             et.commit();
@@ -92,7 +116,7 @@ public class Main {
         try {
             et = em.getTransaction();
             et.begin();
-            User user = em.find(User.class, 1);
+            User user = em.find(User.class, 4);
             em.remove(user);
             et.commit();
         }
