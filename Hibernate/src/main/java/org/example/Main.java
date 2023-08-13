b3a1d609b05b860626464af56188bbb61df4d8fd
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-13 21:47:18 +0200

    hibernate cards and decks one to many

--- Hibernate/src/main/java/org/example/Main.java
+++ Hibernate/src/main/java/org/example/Main.java
@@ -21,57 +21,48 @@ public class Main {
         EntityManager s =  etf.createEntityManager();
 
         s.getTransaction().begin();
-        Data d1 = new Data().setAge("16").setWeigth("77 kg").setPhone("111222333");
-        Data d2 = new Data().setAge("46").setWeigth("27 kg").setPhone("123456789");
 
+        Deck d1 = new Deck().setName("symbioza");
+        Deck d2 = new Deck().setName("niedzwiedzie");
 
-        Apartment a1 = new Apartment();
-        a1.setName("stary Budynek");
-        Apartment a2 = new Apartment();
-        a2.setName("Nowy budynek");
+        s.persist(d1);
+        s.persist(d2);
 
-        Tenant t1 = new Tenant();
-        t1.setName("Sztanga");
-        t1.setData(d1);
-        Tenant t2 = new Tenant();
-        t2.setName("ZIemia");
-        t2.setData(d2);
+        Card c1 = new Card().setName("hemdall").setDeck(List.of(d1, d2));
+        Card c2 = new Card().setName("kurczak").setDeck(List.of(d1));
+        Card c3 = new Card().setName("pieczony").setDeck(List.of( d2));
+        Card c4 = new Card().setName("kotlet").setDeck(List.of());
 
-        a1.setLocators(List.of(t1,t2));
-        a2.setLocators(List.of(t2));
+        s.persist(c1);
+        s.persist(c2);
+        s.persist(c3);
+        s.persist(c4);
 
-        t1.setLivingPlaces(List.of(a1));
-        t2.setLivingPlaces(List.of(a1,a2));
-
-        s.persist(t1);
-        s.persist(t2);
-        s.persist(a1);
-        s.persist(a2);
-        t1.setName("zmieniony po persist");
-
-        s.detach(a1);
-        a1.setName("nie powino sie pokazac to");
-
+        d1.setCards(List.of(c1, c3));
+        d2.setCards(List.of(c2, c4));
 
         s.getTransaction().commit();
         s.getTransaction().begin();
 
-        TypedQuery<Apartment> tq = s.createQuery("SELECT a FROM Apartment a", Apartment.class);
-        List<Apartment> apartments = tq.getResultList();
-        for (Apartment a : apartments) {
-            System.out.println("Locators of apartment with name: " + a.getName());
-            a.getLocators().forEach(l -> System.out.println(l.getName() + l.getData()));
+
+        TypedQuery<Deck> tq = s.createQuery("SELECT d FROM Deck d", Deck.class);
+        List<Deck> decks = tq.getResultList();
+        Deck deck = decks.get(0);
+
+        TypedQuery<Card> tq2 = s.createQuery("SELECT c FROM Card c", Card.class);
+        List<Card> cards = tq2.getResultList();
+        Card card = cards.get(0);
+
+        System.out.println("Card of deck | " + deck.getName() + " | are:");
+        for (int i = 0; i < deck.getCards().size(); i++) {
+            System.out.println(deck.getCards().get(i).getName());
         }
 
-        List<Tenant> tenants = s.createQuery("SELECT t FROM Tenant t", Tenant.class).getResultList();
-        for (Tenant t : tenants) {
-            System.out.println("tenant of name: " + t.getName() + " is living in bildings with name: ");
-            t.getLivingPlaces().forEach(a -> System.out.println(a.getName()));
+
+        System.out.println("Decks that card: | " + card.getName() + " | are: ");
+        for (int i = 0; i < card.getDeck().size(); i++) {
+            System.out.println(card.getDeck().get(i).getName());
         }
-
-
-        Apartment a3 = s.find(Apartment.class, 1);
-        a3.setName("changed");
 
         s.getTransaction().commit();
         s.close();
