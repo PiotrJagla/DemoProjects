2365b042b5030dbb2fe54f7f711ec7d2ce9e6b6a
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-20 15:06:18 +0200

    adding maven modlues and learning

---
+++ MavenPlay/BusinessLogic/src/test/java/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+
+
+    @Test
+    public void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
