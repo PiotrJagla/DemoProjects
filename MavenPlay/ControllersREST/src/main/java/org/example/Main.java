357be9328a55e8780f22b47c5b3b881f30689b9d
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-29 09:29:25 +0200

    coding problem with garbage

--- MavenPlay/ControllersREST/src/main/java/org/example/Main.java
+++ MavenPlay/ControllersREST/src/main/java/org/example/Main.java
@@ -1,9 +1,54 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        Pudlo pudlo = new Pudlo();
+        System.out.println("This is not world");
+        Solution s = new Solution();
+        s.garbageCollection(new String[]{"G","P","GP","GG"}, new int[]{2,4,3});
+    }
+
+}
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+
+        int result = 0;
+        int[] trucks = new int[travel.length];
+        Arrays.fill(trucks, 0);
+        int i = garbage.length - 1;
+        boolean m = false;
+        boolean p= false;
+        boolean g = false;
+        while(true) {
+            if(i-1 < 0) {
+                break;
+            }
+            if(garbage[i].contains("M") || m){
+                trucks[i-1]++;
+                m = true;
+            }
+
+            if(garbage[i].contains("G") || g){
+                trucks[i-1]++;
+                g = true;
+            }
+
+            if(garbage[i].contains("P") || p){
+                trucks[i-1]++;
+                p = true;
+            }
+            --i;
+        }
+
+        result += garbage[0].length();
+        for(int n = 1 ; n < garbage.length ; ++n) {
+            result += travel[n-1] * trucks[n-1];
+            result += garbage[n].length();
+        }
+
+        return result;
     }
 }
--- justTestin/asyncJavascript/async.js
+++ justTestin/asyncJavascript/async.js
@@ -1,26 +1,26 @@
 import fetch from 'node-fetch'
 
-async function boilWater() {
-    setTimeout(() => {
-        console.log("water is boiled")
-    }, 1000)
+
+async function func1() {
+    return setTimeout(() => {
+        console.log("func1");
+
+    },100)
+}
+async function func2() {
+    return setTimeout(() => {
+        console.log("func2");
+
+    },1000)
+}
+async function func3() {
+    return setTimeout(() => {
+        console.log("func3");
+        return 1;
+
+    },6000)
 }
 
-async function afterBoiling() {
-    console.log("after boiling");
-}
 
-async function asyncCall() {
-
-
-    // await boilWater().then(console.log("Costam"))
-    new Promise(console.log("costam"))
-    // await afterBoiling()
-    // for(let i = 0 ; i < 4 ; ++i) {
-    //     await setTimeout(() => {
-    //         console.log(i)
-    //     }, 1000)
-    // }
-}
-
-asyncCall()
+Promise.resolve().then(() => console.log(2))
+console.log(1)
