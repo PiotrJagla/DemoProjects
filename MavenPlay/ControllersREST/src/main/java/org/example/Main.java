b6ba6a10d7456ffed10c9b02bf88c3f4ee3b3095
PiotrJagielny <piotrek2002.09@gmail.com>
2023-08-30 09:40:52 +0200

    messin aroung with java functional programming

--- MavenPlay/ControllersREST/src/main/java/org/example/Main.java
+++ MavenPlay/ControllersREST/src/main/java/org/example/Main.java
@@ -1,54 +1,97 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
-public class Main {
+public class Main{
+
     public static void main(String[] args) {
+        String[] names = new String[]{"piotr", "amelka", "nazwa"};
+        List<String> sss = Arrays.stream(names).filter(n -> n.length() > 3).collect(Collectors.toList());
+        System.out.println(sss);
+        Function<Integer, Integer> fn = n -> {
+            System.out.println("Hello");
+            return  n + 12;
+        };
+        Function<Integer, Integer> fn3 = n -> n+4;
+        fn.apply(4);
 
-        System.out.println("This is not world");
-        Solution s = new Solution();
-        s.garbageCollection(new String[]{"G","P","GP","GG"}, new int[]{2,4,3});
+    }
+}
+
+class CardRepo {
+    public void deleteCard(CardDisplay card) {
+
     }
 
+    public Optional<Integer> getCopies(CardDisplay card) {
+        return Optional.of(card.getCount());
+    }
+    public void addCard(CardDisplay card) {
+
+    }
 }
-class Solution {
-    public int garbageCollection(String[] garbage, int[] travel) {
+class NotBigDeal {
 
-        int result = 0;
-        int[] trucks = new int[travel.length];
-        Arrays.fill(trucks, 0);
-        int i = garbage.length - 1;
-        boolean m = false;
-        boolean p= false;
-        boolean g = false;
-        while(true) {
-            if(i-1 < 0) {
-                break;
-            }
-            if(garbage[i].contains("M") || m){
-                trucks[i-1]++;
-                m = true;
-            }
 
-            if(garbage[i].contains("G") || g){
-                trucks[i-1]++;
-                g = true;
+    private CardRepo cardRepo;
+    private void deleteCard(CardDisplay card) {
+        Optional<Integer> cardCount = cardRepo.getCopies(card);
+        if(cardCount.isPresent()) {
+            if(cardCount.get() == 0) {
+                System.out.println("Nie da sie bo jest 0 kopii");
             }
+            cardRepo.deleteCard(card);
+        }
+    }
 
-            if(garbage[i].contains("P") || p){
-                trucks[i-1]++;
-                p = true;
-            }
-            --i;
+    private void addCard(CardDisplay card) {
+        Optional<Integer> cardCount = cardRepo.getCopies(card);
+        if(cardCount.isPresent()) {
+            System.out.println("Taka karta juz jest");
+        }
+        else {
+            cardRepo.addCard(card);
         }
 
-        result += garbage[0].length();
-        for(int n = 1 ; n < garbage.length ; ++n) {
-            result += travel[n-1] * trucks[n-1];
-            result += garbage[n].length();
-        }
+        checkCard(card, (count) -> {System.out.println(count);return count;}, () -> {cardRepo.addCard(card); return -1;});
 
-        return result;
+    }
+    private int countCards(CardDisplay card) {
+        return checkCard(card, (count) -> count, () -> -1);
+
+    }
+    private int checkCard(CardDisplay card, Function<Integer, Integer> whenIsPresent,  Supplier< Integer> whenIsNotPresent) {
+        Optional<Integer> cardCount = cardRepo.getCopies(card);
+        if(cardCount.isPresent()) {
+            return whenIsPresent.apply(cardCount.get());
+        }
+        else {
+            return whenIsNotPresent.get();
+        }
+    }
+}
+
+class CardDisplay {
+    private String name;
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
