b2646909c7358678c1c717f181c71369f2ab49b9
PiotrJagielny <piotrek2002.09@gmail.com>
2023-09-22 16:21:58 +0200

    playing with mongodb and java

--- JavaTestin/src/main/java/org/example/Main.java
+++ JavaTestin/src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import javax.print.Doc;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class Main {
+
+    private static final String connectionString = "mongodb://localhost:27017";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("test");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("cards");
+
+            Bson bson = BasicDBObject.parse("{'points': 55}");
+            Document doc = collection.find(bson).first();
+            if(doc != null) {
+                System.out.println(doc.toJson());
+            }
+            else {
+                System.out.println("Null");
+            }
+
+
+
+        }
     }
 }
