ac51a278c012f11238e5988cdf313674701c8cc9
PiotrJagielny <piotrek2002.09@gmail.com>
2023-10-10 18:37:51 +0200

    working on some lexers

--- JavaTestin/src/main/java/org/example/Main.java
+++ JavaTestin/src/main/java/org/example/Main.java
@@ -1,78 +1,73 @@
 package org.example;
 
-import java.io.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 enum Token {
-    Expression,
-    Operand,
-    Addition,
-    Multiplication,
-    NONE
+    Sentence,
+    Subj,
+    Art,
+    Noun,
+    Verb,
+    Obj
 }
 
-
 class Node{
-    private Token token;
-    private List<Node> children;
+    Token token;
+    List<Node> children;
 
     public Node(Token token) {
-        children = new ArrayList<>();
         this.token = token;
+        children = new ArrayList<>();
     }
 
-    public void addChild(Node child) {
-        children.add(child);
+    public Token getToken() {
+        return token;
     }
 
     public List<Node> getChildren() {
         return children;
     }
 
-    public Token getToken() {
-        return token;
+    public void addChild(Node child) {
+        children.add(child);
     }
+
 }
 
 public class Main {
     public static void main(String[] args) {
-        List<Token> tokens = new ArrayList<>();
-        try {
-            //Tokenize
-            BufferedReader fileContent = new BufferedReader(new StringReader("4*3+9"));
-            String line;
-            while((line = fileContent.readLine()) != null) {
-                for (char c : line.toCharArray()) {
-                    if(Character.isDigit(c)) {
-                        tokens.add(Token.Operand);
-                    }
-                    else if(c =='+') {
-                        tokens.add(Token.Addition);
-                    }
-                    else if(c =='*') {
-                        tokens.add(Token.Multiplication);
-                    }
-                }
-            }
+        String input = "dog bit cat";
+        List<Token> tokens = tokenize(input);
+        System.out.println(tokens);
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        //Analyze syntactically
         Stack<Token> tokenStack = new Stack<>();
 
 
-
     }
-    private static void traverseTree(Node root) {
 
-        System.out.println(root.getToken());
+    public static List<Token> tokenize(String input) {
+        List<Token> res = new ArrayList<>();
 
-        List<Node> children = root.getChildren();
-        for (int i = 0; i < children.size(); i++) {
-            traverseTree(children.get(i));
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) != ' ') {
+                s.append(input.charAt(i));
+            }
+
+            if(s.toString().equals("dog") || s.toString().equals("cat") || s.toString().equals("man")) {
+                res.add(Token.Noun);
+                s = new StringBuilder();
+            }
+            else if(s.toString().equals("bit") || s.toString().equals("kicked")) {
+                res.add(Token.Verb);
+                s = new StringBuilder();
+            }
         }
+
+        return res;
     }
+
 }
